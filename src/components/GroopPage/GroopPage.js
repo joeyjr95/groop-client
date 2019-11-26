00ce59f89119202c64328729e96575d680d5fd1f
Joey Romo <joeyjr95@gmail.com>
2019-11-26 16:59:10 -0500

    groop desktop

--- src/components/GroopPage/GroopPage.js
+++ src/components/GroopPage/GroopPage.js
@@ -4,28 +4,77 @@ export default class GroopPage extends Component {
 render(){
     return (
     <section className="groop-page-list">
-        <div>
+        
+        <div className="members-section">
+            <div className="members">
+            <label htmlFor="menu" id="label-menu">Members</label>
             <ul className="menu" role="menu">
             <li   id="name1">User</li>
             <li   id="name2">Allie</li>
             <li   id="name3">Derek</li>
             <li   id="name4">Brian</li>
             </ul>
+            </div>
+            <div className ="scores-section">
+            <div className="scores-section1">
+            <label htmlFor="weekly-scores" id="weekly-scores-label">Top Scores for this week</label>
+            <ol className="weekly-scores" >
+            <li   id="name1">User: 22</li>
+            <li   id="name2">Allie: 17</li>
+            <li   id="name3">Derek: 9</li>
+            </ol>
+            </div>
+            <div className="scores-section2">
+            <label htmlFor="total-scores" id="total-scores-label">Top Scores all time</label>
+            <ol className="total-scores" >
+            <li   id="name1">User: 122</li>
+            <li   id="name2">Allie: 117</li>
+            <li   id="name3">Derek: 91</li>
+            </ol>
+            </div>
+            </div>
         </div>
         <div className="task-list-container">
+        <div id="fixed-container">
+        <label htmlFor="task-list" id="label-task-list">Today's tasks</label>
+        </div>
             <ul className="task-list">
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
-                <li>task</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
             </ul>
 
 
--- src/routes/GroupPageRoute/GroupPageRoute.js
+++ src/routes/GroupPageRoute/GroupPageRoute.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import './GroupPageRoute.css'
 import GroopPage from "../../components/GroopPage/GroopPage";
 
@@ -8,6 +9,8 @@ render(){
     <h2>groop title</h2>
     <p id="date">today's date</p>
     < GroopPage />
+    {/* add in the correct link below */}
+    <Link to="/newtask" id="task-link">Add to list</Link>
     </div>
     )
 }
