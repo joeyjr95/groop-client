175aa33582e5a8c7671347ad4a06eac694cb3c6d
Joey Romo <joeyjr95@gmail.com>
2019-11-26 21:49:43 -0500

    css changed to scss for group page

--- src/routes/GroupPageRoute/GroupPageRoute.js
+++ src/routes/GroupPageRoute/GroupPageRoute.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-import './GroupPageRoute.css'
+import './GroupPageRoute.scss'
 import GroopPage from "../../components/GroopPage/GroopPage";
 
 export default class GroupPageRoute extends Component {
