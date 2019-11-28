83b00f8333e757a84ed5aa125252612308c67b81
Joey Romo <joeyjr95@gmail.com>
2019-11-28 18:07:33 -0500

    gets all tasks on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,73 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
-
-
+import GroopContext from "../../contexts/GroopContext";
+import GroopService from "../../services/groop-service";
 
 export default class Dashboard extends Component {
-    render(){
-        return(
-        <section className="dashboard-c">
-                <h2>My Taskboard</h2>
-                <p id="date">Today's date</p>
-                <div className="groups">
-            <label htmlFor="group-menu" id="label-group-menu">Members</label>
-            <ul className="group-menu" role="menu">
-            <li   id="group1">Group1</li>
-            <li   id="group2">Group2</li>
-            <li   id="group3">Group3</li>
-            <li   id="group4">Group4</li>
-            </ul>
-            </div>
+  static contextType = GroopContext;
+  componentDidMount() {
+      GroopService.getAllTasks()
+      .then( data => {
+            this.context.setUserTasks(data)
+      })
+
+  }
+  render() {
+      
+    const { userTasks = []} = this.context;
+    console.log(userTasks)
+    return (
+      <section className="dashboard-c">
+        <h2>My Taskboard</h2>
+        <p id="date">Today's date</p>
+        <div className="groups">
+          <label htmlFor="group-menu" id="label-group-menu">
+            Members
+          </label>
+          <ul className="group-menu" role="menu">
+            <li id="group1">Group1</li>
+            <li id="group2">Group2</li>
+            <li id="group3">Group3</li>
+            <li id="group4">Group4</li>
+          </ul>
+        </div>
         <div className="dashboard-task-list-container">
-            <div id="dashboard-fixed-container">
-            <label htmlFor="dashboard-task-list" id="dashboard-label-task-list">Today's tasks</label>
+          <div id="dashboard-fixed-container">
+            <label htmlFor="dashboard-task-list" id="dashboard-label-task-list">
+              Today's tasks
+            </label>
+          </div>
+          <ul className="dashboard-task-list">
+          {userTasks.map(task => (
+              <li key={task.id} id={task.id} aria-live="polite">
+                <p>{task.name}: {task.description}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-            <ul className="dashboard-task-list">
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </li>
-            </ul>
 
-
-        </div>
-        
-        <Link id="group-link" to="/hub">Groups</Link>
-            </section>
-        )
-    }
-
+        <Link id="group-link" to="/hub">
+          Groups
+        </Link>
+      </section>
+    );
+  }
 }
