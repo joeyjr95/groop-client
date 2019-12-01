c4929796052e9f8e7677d7fecff17dfdcdb491b3
Quasar Wei <quasar.wei@outlook.com>
2019-12-01 05:07:48 -0800

    import app after normalizecss in index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import { GroopProvider } from "./contexts/GroopContext";
-import App from "./components/App/App";
-import { UserProvider } from "./contexts/UserContext";
+import { BrowserRouter } from 'react-router-dom';
+import { GroopProvider } from './contexts/GroopContext';
+import { UserProvider } from './contexts/UserContext';
 import * as serviceWorker from './serviceWorker';
 
-import 'normalize.css'
+import 'normalize.css';
 import './index.scss';
+import App from './components/App/App';
 
 ReactDOM.render(
-<BrowserRouter>
+  <BrowserRouter>
     <UserProvider>
       <GroopProvider>
-     
         <App />
-        </GroopProvider>
+      </GroopProvider>
     </UserProvider>
-  </BrowserRouter>, document.getElementById('root'));
-
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
