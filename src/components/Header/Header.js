9cafaf4cc794efe45d5928203fab9b3e300437a7
Brock Boutwell <brockboutwell@gmail.com>
2019-12-04 11:27:56 -0500

    added settings cog and link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import TokenService from "../../services/token-service";
 import UserContext from "../../contexts/UserContext";
 import "./Header.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+
 
 class Header extends Component {
   static contextType = UserContext;
@@ -15,7 +18,8 @@ class Header extends Component {
     return (
       <div className="link-logout" aria-live="polite">
         <span className="div-span">{this.context.user.username}</span>
-        
+        <Link to="/settings"><FontAwesomeIcon icon={faCog} id="settingsIcon"/>
+        </Link>
         <nav>
           <Link
             onClick={this.handleLogoutClick}
