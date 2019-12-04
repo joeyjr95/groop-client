fa77a58de583ea94836247d9a4fb9c5620aec6c4
Joey Romo <joeyjr95@gmail.com>
2019-12-04 11:34:02 -0500

    changes to sidebar and groopage

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,73 +1,90 @@
 import React, { Component } from "react";
 import GroopContext from "../../contexts/GroopContext";
-import './Filter.scss'
+import "./Filter.scss";
 export default class Filter extends Component {
   static contextType = GroopContext;
   state = {
     selectedUser: ""
   };
 
-
-  filterTasksByUser = (e) => {
-      e.preventDefault()
-    let group = this.context.currentGroupMembers
+  filterTasksByUser = e => {
+    e.preventDefault();
+    let group = this.context.currentGroupMembers;
     let groupTasks = this.context.currentGroupTasks;
     let selectedUser = this.state.selectedUser;
-    let user = group.find( u => u.username === this.state.selectedUser)
-
-    if(!selectedUser){
-      this.context.setFilteredTasks(groupTasks)
-    }else if(!user){
-      this.context.setFilteredTasks(groupTasks)
-      alert('user not in group')
+    let user = group.find(u => u.username === this.state.selectedUser);
+    console.log(groupTasks)
+    if (!selectedUser) {
+      this.context.setFilteredTasks(groupTasks);
+    } else if (!user) {
+      this.context.setFilteredTasks(groupTasks);
+      alert("user not in group");
       this.setState({
         selectedUser: ""
-    })
-    }else if(user.username === selectedUser){
+      });
+    } else if (user.username === selectedUser) {
       let filterTasks = groupTasks.filter(tasks => {
-        return tasks.user_assigned_id == user.member_id
-          })
-      this.context.setFilteredTasks(filterTasks)
-     }
-        
-    
-     
+        return tasks.user_assigned_id == user.member_id;
+      });
+      this.context.setFilteredTasks(filterTasks);
+    }
   };
 
-
-  onSelectChange = (e) => {
-      this.setState({
-          selectedUser: e
-      })
-      
-  }
-  onReset = (e) =>{
+  searchDescription = e =>{
+    e.preventDefault();
     let groupTasks = this.context.currentGroupTasks;
-    e.preventDefault()
+    
+    let selectedUser = this.state.selectedUser;
+    let filterTasks = groupTasks.filter(tasks => {
+      return tasks.description.includes(selectedUser)  
+    });
+    console.log(filterTasks)
+    this.context.setFilteredTasks(filterTasks);
+  }
+  searchTaskName = e =>{
+    e.preventDefault();
+    let groupTasks = this.context.currentGroupTasks;
+    
+    let selectedUser = this.state.selectedUser;
+    let filterTasks = groupTasks.filter(tasks => {
+      return tasks.name.includes(selectedUser)  
+    });
+    console.log(filterTasks)
+    this.context.setFilteredTasks(filterTasks);
+  }
+  // searchUsername = e =>
+
+  onSelectChange = e => {
+    this.setState({
+      selectedUser: e
+    });
+  };
+  onReset = e => {
+    let groupTasks = this.context.currentGroupTasks;
+    e.preventDefault();
     this.setState({
       selectedUser: ""
-  })
-  this.context.setFilteredTasks(groupTasks)
-  
-  }
-
+    });
+    this.context.setFilteredTasks(groupTasks);
+  };
 
   render() {
-    console.log(this.context.filteredTasks)
+    console.log(this.context.filteredTasks);
     return (
       <div className="filter">
         <label htmlFor="member-select"> Search Tasks by User:</label>
-        <form className="member-select"  >
-        <input type="text"
+        <form className="member-select">
+          <input
+            type="text"
             id="member-select"
             name="member-select"
             placeholder="enter username here"
             value={this.state.selectedUser}
-            onChange={(e) => this.onSelectChange(e.target.value)}/>
-            <button onClick={(e) =>this.filterTasksByUser(e)}>Search</button>
-            <button onClick={(e) =>this.onReset(e)}>Clear</button>
+            onChange={e => this.onSelectChange(e.target.value)}
+          />
+          <button onClick={e => this.filterTasksByUser(e)}>Search</button>
+          <button onClick={e => this.onReset(e)}>Clear</button>
         </form>
-        
       </div>
     );
   }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -23,7 +23,9 @@ export default class Sidebar extends Component {
         <div className="sidenav">
         <div className='flexbox-container'>
         <Link to="/dashboard">Dashboard</Link>
-        <Link to="/hub">Groops</Link>
+        <Link to={`/groupsettings/${this.props.match.params.group_id}`}>
+          Group Settings
+        </Link>
         <Link to={`/add-task/${this.props.match.params.group_id}`}>Add Task</Link>
         <Link to="/settings">Settings</Link>
         </div>
