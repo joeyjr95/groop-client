fe843c919918bc6fb3f94d46267760c0de4e7d55
Joey Romo <joeyjr95@gmail.com>
2019-12-04 15:30:01 -0500

    accidentally worked on development

--- src/components/GroopPage/GroopPage.js
+++ src/components/GroopPage/GroopPage.js
@@ -1,12 +1,12 @@
 /// <reference path='../../react-vis.d.ts'/>
-import GroopContext from '../../contexts/GroopContext';
-import GroopService from '../../services/groop-service';
-import Filter from '../../components/Filter/Filter'
-import React, { Component } from 'react';
-import { RadialChart } from 'react-vis';
-import TaskItem from '../TaskItem/TaskItem';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMedal } from '@fortawesome/free-solid-svg-icons'
+import GroopContext from "../../contexts/GroopContext";
+import GroopService from "../../services/groop-service";
+import Filter from "../../components/Filter/Filter";
+import React, { Component } from "react";
+import { RadialChart } from "react-vis";
+import TaskItem from "../TaskItem/TaskItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMedal } from "@fortawesome/free-solid-svg-icons";
 
 export default class GroopPage extends Component {
   static contextType = GroopContext;
@@ -23,25 +23,26 @@ export default class GroopPage extends Component {
   getGroupTasks = () => {
     GroopService.getGroupTasks(this.props.group_id).then(data => {
       this.context.setCurrentGroupTasks(data);
-      this.context.setFilteredTasks(data)
-      
+      this.context.setFilteredTasks(data);
     });
   };
 
   getGroupMembers = () => {
     GroopService.getGroupMembers(this.props.group_id).then(data => {
       this.context.setCurrentGroupMembers(data);
-      
     });
   };
-  
 
   render() {
-    const { currentGroupTasks = [], currentGroupMembers = [], filteredTasks =[] } = this.context;
-    console.log(filteredTasks)
+    const {
+      currentGroupTasks = [],
+      currentGroupMembers = [],
+      filteredTasks = []
+    } = this.context;
+    console.log(filteredTasks);
     return (
       <>
-      <Filter/>
+        <Filter />
         <div className="members-section-mobile">
           <div className="members-mobile">
             <label htmlFor="menu" id="label-menu">
@@ -73,8 +74,10 @@ export default class GroopPage extends Component {
                     id={member.member_id}
                     aria-live="polite"
                   >
-                    {member.username}<br />
-                    <FontAwesomeIcon icon={faMedal} id="pointsIcon"/> 3{member.points}
+                    {member.username}
+                    <br />
+                    <FontAwesomeIcon icon={faMedal} id="pointsIcon" /> 3
+                    {member.points}
                   </li>
                 ))}
               </ul>
@@ -103,20 +106,20 @@ export default class GroopPage extends Component {
             </div>
             <div className="pieChart">
               <RadialChart
-                colorType={'literal'}
+                colorType={"literal"}
                 colorDomain={[0, 100]}
                 colorRange={[0, 10]}
                 getLabel={d => d.name}
                 data={[
-                  { angle: Number(17), color: '#1c939a', name: 'allie' },
-                  { angle: Number(22), color: '#72bce0', name: 'User' },
-                  { angle: Number(9), color: '#BAD7E6', name: 'Derek' },
-                  { angle: Number(5), color: '#5891AD', name: 'Brian' },
+                  { angle: Number(17), color: "#1c939a", name: "allie" },
+                  { angle: Number(22), color: "#72bce0", name: "User" },
+                  { angle: Number(9), color: "#BAD7E6", name: "Derek" },
+                  { angle: Number(5), color: "#5891AD", name: "Brian" }
                 ]}
                 labelsRadiusMultiplier={1}
                 labelsStyle={{ fontSize: 16 }}
                 showLabels
-                style={{ stroke: '#fff', strokeWidth: 2 }}
+                style={{ stroke: "#fff", strokeWidth: 2 }}
                 width={window.innerWidth / 5}
                 height={window.innerWidth / 5}
               ></RadialChart>
@@ -130,18 +133,17 @@ export default class GroopPage extends Component {
               </label>
             </div>
             <ul className="task-list">
-              {
-                  filteredTasks.map((task, i) => {
-                    console.log(task)
-                     return <TaskItem
-                      getTasks={() => this.getGroupTasks()}
-                      task={task}
-                      {...this.props}
-                      key={`task${i}`}
-                    />
-                  })}
-              
-             
+              {filteredTasks.map((task, i) => {
+                console.log(task);
+                return (
+                  <TaskItem
+                    getTasks={() => this.getGroupTasks()}
+                    task={task}
+                    {...this.props}
+                    key={`task${i}`}
+                  />
+                );
+              })}
             </ul>
           </div>
         </section>
