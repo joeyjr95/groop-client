04c8ff4fca1bdf1ccb9d13e0d5b7c8cce4481263
Joey Romo <joeyjr95@gmail.com>
2019-12-05 17:54:45 -0500

    fixed calendar

--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -27,7 +27,7 @@ class Calender extends Component{
     events: []
   };
   addDays(date, days) {
-    var result = new Date(date);
+    let result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
   }
