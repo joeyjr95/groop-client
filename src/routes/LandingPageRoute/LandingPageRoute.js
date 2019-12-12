90ebb5f3233d8c6f8b3f984c15bde761be28b91a
Quasar Wei <quasar.wei@outlook.com>
2019-12-12 13:28:32 -0800

    landing layout

--- src/routes/LandingPageRoute/LandingPageRoute.js
+++ src/routes/LandingPageRoute/LandingPageRoute.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./LandingPageRoute.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
-import RegisterImage from '../../images/register.png'
-import DashboardImage from '../../images/dashboard.png'
-import GroupDashboardImage from '../../images/groupdashboard.png'
-import GroupSettingsImage from '../../images/groupsettings.png'
-import SettingsImage from '../../images/accountsettings.png'
-
-
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './LandingPageRoute.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import DashboardImage from '../../images/dashboard.png';
+import GroupDashboardImage from '../../images/groupdashboard.png';
+import GroupSettingsImage from '../../images/groupsettings.png';
+import taskitemimg from '../../images/taskitem.jpg';
+import searchfilterimg from '../../images/searchfilter.jpg';
+import sidebarimg from '../../images/sidebar.jpg';
 
 export default class LandingPageRoute extends Component {
   render() {
@@ -29,102 +28,92 @@ export default class LandingPageRoute extends Component {
         </section>
         <section id="About"></section>
         <section className="AboutContainer">
-          <div className="SectionAboutCont">
-            <h2>Who we are</h2>
-            <ul>
-              <li>Quasar Wei</li>
-              <li>Brock Boutwell</li>
-              <li>Joey Romo</li>
-              <li>Laura Elias</li>
-            </ul>
+          <div className=" feature-section--about">
             <p className="Aboutdescription">
-              This is a collaborative project by 4 aspiring developers.
-              Our intention was to create an app that can be used for a wide
-              variety of collaboration and event organization purposes, while being
-              fun at the same time! Once logged in and registered, a user can
-              create groups, and within those groups invite/remove members,
-              create/edit tasks, and view a scoreboard to effectively get tasks done on
-              time.
+              Groop is an an app that can be used for a variety of collaboration
+              and event organization purposes while being fun at the same time!
+              Once logged in and registered, a user can create groups, and
+              within those groups invite/remove members, create/edit tasks, and
+              view a scoreboard to effectively get tasks done on time. Keep
+              track of tasks with optional email notifications.
             </p>
-            <h3> How to use groop</h3>
           </div>
-          <div className="SectionAboutCont">
-            <h2>Register and Login</h2>
-            <div className="MarryContainer">
-              <img alt="Register/Login" src={RegisterImage} />
-              <div className="TutInnerCont">
-                <p>Choose a Name</p>
-                <p>Choose a UserName</p>
-                <p>Choose your Email Address</p>
-                <p>Choose a Password</p>
-                <p>Verify the Password</p>
-                <p>Verify and check your email upon registering</p>
-                <p>Login with a newly registered account</p>
-              </div>
+          <div className="feature-section">
+            <img alt="create groops" src={DashboardImage} />
+            <div className="feature-description">
+              <h2>Dashboard</h2>
+              <p>See all upcoming tasks you are assigned to in the dashboard</p>
             </div>
           </div>
-          <div className="SectionAboutCont">
-            <h2>Dashboard</h2>
-            <div className="MarryContainer">
-              <img alt="create groops" src={DashboardImage} />
-              <div className="TutInnerCont">
-                <p>Search/Filter tasks</p>
-                <p>Edit/See more info of Tasks Assigned to you</p>
-
-                <h3>Sidebar Menu</h3>
-                <p>Logout</p>
-                <p>Settings</p>
-                <p>Close the menu</p>
-                <p>Create groops</p>
-                <p>View/Go to groops page</p>
-                <p>View Calendar</p>
-              </div>
+          <div className="feature-section">
+            <img alt="create groops" src={taskitemimg} />
+            <div className="feature-description">
+              <h2>Task Items</h2>
+              <p>
+                Check the priority and due date of a task at a glance. Quickly
+                mark tasks as complete, show more information, or edit a task.
+              </p>
             </div>
           </div>
-          <div className="SectionAboutCont">
-            <h2>Groops Dashboard</h2>
-            <div className="MarryContainer">
+          <div className="feature-section feature-section--filter">
+            <img alt="create groops" src={searchfilterimg} />
+            <div className="feature-description">
+              <h2>Search</h2>
+              <p>Search for tasks using different filters and a search query</p>
+            </div>
+          </div>
+          <div className="feature-section">
+            <img alt="create groops" src={sidebarimg} />
+            <div className="feature-description">
+              <h2>Sidebar</h2>
+              <p>
+                Group owners can access group settings on the respective group
+                page. Members can add tasks to a group while on the group page.
+                Navigate back to the dashboard from a group page, or to a
+                different group from the dashboard. Access a calendar and log
+                out.
+              </p>
+            </div>
+          </div>
+          <div className="feature-section">
+            <div className="img-container">
               <img alt="create groops" src={GroupDashboardImage} />
-              <div className="TutInnerCont">
-                <p>Search/Filter tasks</p>
-                <p>View Members of a group</p>
-                <p>Edit/See more info of Tasks in your groop</p>
-
-                <h3>Sidebar Menu</h3>
-                <p>Logout</p>
-                <p>Settings</p>
-                <p>Groop Settings</p>
-                <p>Close the Menu</p>
-                <p>Create Tasks</p>
-                <p>View Groop Calendar</p>
-              </div>
+            </div>
+            <div className="feature-description">
+              <h2>Groops Dashboard</h2>
+              <p>Manage tasks on your group page</p>
             </div>
           </div>
-          <div className="SectionAboutCont">
-            <h2>Settings</h2>
-            <div className="MarryContainer">
-              <img alt="create groops" src={SettingsImage} />
-              <div className="TutInnerCont">
-                <p>Edit/Change Email Address</p>
-                <p>Edit/Change Password</p>
-                <p>Manage Email Notifications</p>
-                <p>Edit/Change email address</p>
-              </div>
-            </div>
-          </div>
-          <div className="SectionAboutCont">
-            <h2>Group Settings</h2>
-            <div className="MarryContainer">
+          <div className="feature-section">
+            <div className="img-container">
               <img alt="create groops" src={GroupSettingsImage} />
-              <div className="TutInnerCont">
-                <p>Create Categories for Tasks</p>
-                <p>Add/Remove Members from groop</p>
-                <p>Delete groop entirely</p>
-              </div>
+            </div>
+            <div className="feature-description">
+              <h2>Group Settings</h2>
+              <p>Manage your group members and categories</p>
+            </div>
+          </div>
+          <div className="feature-section">
+            <div className="img-container">
+              <img alt="create groops" src={GroupSettingsImage} />
+            </div>
+            <div className="feature-description">
+              <h2>Email Notifications</h2>
+              <p>
+                Stay up to date with email notifications and a weekly reminder
+              </p>
             </div>
           </div>
         </section>
         <div className="landing-page-bg"></div>
+        <div className=" feature-section--about">
+          <ul>
+            <li>Quasar Wei</li>
+            <li>Brock Boutwell</li>
+            <li>Joey Romo</li>
+            <li>Laura Elias</li>
+          </ul>
+        </div>
       </>
     );
   }
