e2bd210867bc5a71f29c78c2ef29d6e57eaa6f98
Quasar Wei <quasar.wei@outlook.com>
2019-12-13 08:25:43 -0800

    reset commit 6ec0586

---
+++ src/components/Button/Buttone.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import Button from './Button';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Router><Button /></Router>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
